d418178fd85592026e2d02ac2f7daea295a11c4d
Golu <anurag.bhardwaj.cpp@gmail.com>
2020-05-20 11:03:41 +0530

    Updates

---
+++ precache-manifest.6cf229cf11540711d53a5520b5f29ac9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b6a3affe4f592d017a55ba1fa491dd7",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "1177b14f6dd2aa445989",
+    "url": "/contactmanager/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "f273ec7dffff72e03313",
+    "url": "/contactmanager/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "1177b14f6dd2aa445989",
+    "url": "/contactmanager/static/js/2.8ef55e31.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/contactmanager/static/js/2.8ef55e31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f273ec7dffff72e03313",
+    "url": "/contactmanager/static/js/main.2294f24f.chunk.js"
+  },
+  {
+    "revision": "3959f31844f85f026a86",
+    "url": "/contactmanager/static/js/runtime-main.711199b3.js"
+  }
+]);
